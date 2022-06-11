1e35398a201ab9519ff40fb181bdf59edf9a3128
Juan Villela <juan@villela.co>
2022-06-11 18:41:08 -0400

    feat: Add login/logout button to header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
+import { useUser } from '@auth0/nextjs-auth0';
+
 import { config } from '@/utils/AppConfig';
 
 const Header = () => {
+  const { user } = useUser();
+
   const nav: { [key: string]: string } = config.nav.header;
   const navItems = Object.keys(nav);
 
@@ -9,7 +13,7 @@ const Header = () => {
       <section className="col-start-2 flex items-center justify-between row-start-2 w-full">
         <a
           href={config.baseURL}
-          className="site-name focus:no-underline focus:text-secondary font-mdNichrome hover:no-underline hover:text-secondary leading-5 no-underline text-primary text-xl xxs:leading-none xxs:text-2xl"
+          className="site-name focus:no-underline focus:text-secondary font-mdNichrome hover:no-underline hover:text-secondary leading-5 no-underline text-primary text-xl xxs:leading-none xxs:text-2xl whitespace-nowrap"
         >
           {config.title}
         </a>
@@ -25,6 +29,27 @@ const Header = () => {
                 </a>
               </li>
             ))}
+            <li className="ml-2 my-0 row-start-1">
+              <a
+                className="
+                    bg-secondary
+                    duration-300
+                    ease-in-out
+                    hover:bg-primary
+                    hover:no-underline
+                    hover:text-background
+                    no-underline
+                    px-2
+                    py-0
+                    rounded
+                    text-background
+                    transition-colors
+                  "
+                href={`/api/auth/${user ? 'logout' : 'login'}`}
+              >
+                {user ? 'Logout' : 'Login'}
+              </a>
+            </li>
           </ul>
         </nav>
       </section>
