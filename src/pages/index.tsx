702ccbf9d38c3db43ede670e11da67b2c80da828
Juan Villela <juan@villela.co>
2022-06-11 20:52:12 -0400

    feat: Route to bookmarks page on login.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { useUser } from '@auth0/nextjs-auth0';
 
-import Intro from '@/components/Intro';
 import { Main } from '@/templates/Main';
 import NoAuth from '@/components/NoAuth';
 
 const Home = () => {
+  const router = useRouter();
   const { user, error, isLoading } = useUser();
 
+  useEffect(() => {
+    if (user) {
+      router.push('/bookmarks');
+    }
+  }, [router, user]);
+
   return (
-    <Main>
-      {user ? <Intro /> : <NoAuth error={error} isLoading={isLoading} />}
-    </Main>
+    <Main>{user ? null : <NoAuth error={error} isLoading={isLoading} />}</Main>
   );
 };
 
