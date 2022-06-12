a4d0a0d6debafa575cc0f76df15d92a2bdefbab3
Juan Villela <juan@villela.co>
2022-06-11 20:52:28 -0400

    feat: Add initial table component.

---
+++ src/components/Table.tsx
@@ -0,0 +1,50 @@
+import { useTable } from 'react-table';
+
+import { Record } from '@/utils/types';
+
+interface TableProps {
+  data: Record[];
+  columns: Column[];
+}
+
+const Table = ({ columns, data }: TableProps) => {
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({
+      columns,
+      data,
+    });
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th key={column.id} {...column.getHeaderProps()}>
+                {column.render('Header')}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row);
+          return (
+            <tr key={row.id} {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return (
+                  <td key={cell.value} {...cell.getCellProps()}>
+                    {cell.render('Cell')}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
